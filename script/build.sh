1dbe7afd19f4fd6cd7948e10808891c7e82fd32c
gunt <0408gun@gmail.com>
2021-01-26 22:29:45 +0900

    [feat] CI 테스트

--- script/build.sh
+++ script/build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 echo "kakao.api.key=$KAKAO_API_KEY" >> ./key.api.properties
-echo "============= ./gradlew cDAT 시작 Unit Test =============";
-./gradlew cDAT
-if [ "$?" -eq "0" ]; then
-	echo "============= ./gradlew build 시작 =============";
-	./gradlew build;
-	if [ "$?" -eq "0" ]; then
-    	exit 0;
-	else
-    	exit 1;
-    	echo "=============  ./gradlew build 실패 =============";
-	fi
-else
-	echo "=============  ./gradlew cDAT 실패 =============";
-	exit 1;
-fi
+./gradlew clean build
