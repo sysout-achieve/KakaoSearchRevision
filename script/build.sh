d4f69e747a3b0dbe0b13d86f1a0282ba564a9293
gunt <0408gun@gmail.com>
2021-01-26 19:01:26 +0900

    [feat] CI 환경 구성

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,17 @@
-- echo "kakao.api.key=$KAKAO_API_KEY" >> ./key.api.properties
-- echo ./gradlew build
+#!/bin/sh
+echo "kakao.api.key=$KAKAO_API_KEY" >> ./key.api.properties
+echo "============= ./gradlew cDAT 시작 Unit Test =============";
+./gradlew cDAT
+if [ "$?" -eq "0" ]; then
+	echo "============= ./gradlew build 시작 =============";
+	./gradlew build;
+	if [ "$?" -eq "0" ]; then
+    	exit 0;
+	else
+    	exit 1;
+    	echo "=============  ./gradlew build 실패 =============";
+	fi
+else
+	echo "=============  ./gradlew cDAT 실패 =============";
+	exit 1;
+fi
